00c08ce16a33b791d1551b57793a015a02516000
hjs6877 <hjs6877@gmail.com>
2022-06-03 13:33:47 +0900

    Complete Pagination

--- src/main/java/com/codestates/member/repository/MemberRepository.java
+++ src/main/java/com/codestates/member/repository/MemberRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
-// TODO 페이지네이션을 적용하세요!
+// 페이지네이션을 적용 완료!
 public interface MemberRepository extends PagingAndSortingRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 }
