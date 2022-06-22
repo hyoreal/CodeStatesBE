71f82ba7e5e7fc02a5fc7037cc7fd3e675f64161
hjs6877 <hjs6877@gmail.com>
2022-06-23 01:21:35 +0900

    Distributed Transaction

---
+++ src/main/java/com/codestates/Section3Week3DistributedTxApplication.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section3Week3DistributedTxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section3Week3DistributedTxApplication.class, args);
+	}
+
+}
