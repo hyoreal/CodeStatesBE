ab462efb15aa106879d34d6a33cdcf50456be990
hjs6877 <hjs6877@gmail.com>
2022-07-04 16:05:54 +0900

    Modify some classes

--- src/main/java/com/codestates/order/dto/OrderCoffeeResponseDto.java
+++ src/main/java/com/codestates/order/dto/OrderCoffeeResponseDto.java
@@ -1,11 +1,10 @@
 package com.codestates.order.dto;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
+@Builder
 @Getter
-@AllArgsConstructor
 public class OrderCoffeeResponseDto {
     private long coffeeId;
     private String korName;
