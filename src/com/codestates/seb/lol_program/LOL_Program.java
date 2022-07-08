ae670f82dc60490d1e4f4c1693970ba34cff8b46
hyojin <hyoreal51@gmail.com>
2022-07-08 14:09:04 +0900

    TRPG

--- src/com/codestates/seb/lol_program/LOL_Program.java
+++ src/com/codestates/seb/lol_program/LOL_Program.java
@@ -3,5 +3,25 @@ package com.codestates.seb.lol_program;
 public class LOL_Program {
   public static void main(String[] args) {
     //TODO:
+    System.out.println("[안내] TPRG 스타크래프트 시작합니다.");
+
+    System.out.println("[안내] 자신의 유닛 정보를 입력해 주세요.");
+
+    LoL_char user1 = new LoL_char(); //유저자신의 정보받기
+    String[] user1info = user1.user_create(); // 유닛생성메소드
+    user1.user_print(user1info);
+
+    System.out.println("========================================");
+
+    System.out.println("[안내] 상대 유닛 정보를 입력해 주세요.");
+    LoL_char user2 = new LoL_char();
+    String[] user2info = user2.user_create();
+    user2.user_print(user2info);
+    System.out.println("========================================");
+
+
+    //공격
+    user1.attack(user1.user_info_int(user1info), user2.user_info_int(user2info) );
+
   }
 }
--- src/com/codestates/seb/lol_program/LoL_char.java
+++ src/com/codestates/seb/lol_program/LoL_char.java
@@ -13,32 +13,57 @@ class LoL_char {
   // 사용자의 입력을 받기위한 객체 생성을 진행합니다.
 
   // 게임에 필요한 변수 타입을 사전 정의합니다.
+  String name;
+  String ad;
+  String def;
+  String hp;
 
   /**
    * @user_info : 생성된 유닛의 정보를 담는 메서드
    * @this : 해당 메서드가 입력받은 변수들은 this 를
-   * */
+   */
   void user_info(String name, String ad, String def, String hp) {
+    this.name = name;
+    this.ad = ad;
+    this.def = def;
+    this.hp = hp;
   }
+
 
   /**
    * @user_create() : 유닛을 생성하는 메서드이며, 입력된 값은 user_info() 메서드에게 전달 및 반환 처리합니다.
    * 반환된 값은 시스템 정보창에 출력되도록 합니다.
-   * */
+   */
   String[] user_create() {
     //TODO:
     //입력 메시지와 함께, 게임에 필요한 유닛의 정보를 입력받습니다.
+    Scanner scanner = new Scanner(System.in); //입력가능하게
+    System.out.print("[시스템] 유닛 [이름] 을 입력해 주세요 : ");
+    String name = scanner.next();
+    System.out.print("[시스템] 유닛 [공격력] 을 입력해 주세요 : ");
+    String ad = scanner.next();
+    System.out.print("[시스템] 유닛 [방어력] 을 입력해 주세요 : ");
+    String def = scanner.next();
+    System.out.print("[시스템] 유닛 [체력] 을 입력해 주세요 : ");
+    String hp = scanner.next();
 
     // 입력된 값은 user_info()에 전달하여 줍니다.
+    user_info(name, ad, def, hp);
 
     // 유저마다의 객체 생성을 위해 반환 과정 또한 정의하여 줍니다.
+    return new String[]{name, ad, def, hp};
   }
 
   /**
    * @user_print() : 입력된 배열을 통해 유닛 정보 출력
-   * */
+   */
   void user_print(String[] user) {
     //TODO:
+    System.out.println("[안내] 생성된 유닛 정보는 다음과 같습니다.");
+    System.out.println("[안내] " + user[0] + " 유닛이 게임에 참여하였습니다.");
+    System.out.println("[공격력] : " + user[1]);
+    System.out.println("[방어력] : " + user[2]);
+    System.out.println("[체력] : " + user[3]);
   }
 
   /**
@@ -48,19 +73,41 @@ class LoL_char {
   int[] user_info_int(String[] info) {
     //TODO:
     // 같은 형식의 반환을 위해 배열 선언
-
+    int[] infoInt = new int[3];
     // 배열의 값을 하나씩 꺼내 정수형태로 전환합니다.
-
+    for (int i = 0; i < infoInt.length; i++) {
+      infoInt[i] = Integer.parseInt(info[i + 1]);
+    }
+    return infoInt;
   }
 
 
   /**
    * @attack() : 공격을 수행하는 메서드
    * 아군 유닛 정보와 상대의 정보를 입력받습니다.
-   * */
+   */
   void attack(int[] me_info_int, int[] enemy) {
     // 조건 1. 적군의 체력이 0 이하면 [유닛 제거] 가 됩니다.
     // 조건 2. 적군 체력이 0 이하가 아니라면 공격을 성공적으로 수행합니다.
     // 공격 정책 수식 : 적군 체력 -= 아군 유닛 공격력 / 적군 유닛 방어력
     //TODO:
+    do {
+      System.out.println("----------------------------------------");
+      System.out.println("[안내] [" + name + "]유닛이 [공격] 하였습니다.");
+
+      if (enemy[2] > 0) {
+        enemy[2] -= me_info_int[0] / enemy[1];
+        System.out.println("[안내] 상대 유닛의 남은 [체력]은 " + enemy[2] + " 입니다.");
+      } else {
+        System.out.println("----------------------------------------");
+        System.out.println("[안내] 더 이상 공격할 수 없습니다.");
+        System.out.println("[안내] 상대 유닛이 제거되었습니다.");
+      }
+    } while (enemy[2] > 0);
+    System.out.println("----------------------------------------");
+    System.out.println("[안내] 더 이상 공격할 수 없습니다.");
+    System.out.println("\n");
+    System.out.println("[안내] 상대 유닛이 제거되었습니다.");
+
+  }
 }
