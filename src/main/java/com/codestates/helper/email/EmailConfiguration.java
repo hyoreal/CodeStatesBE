c7088895319de5d446a5163fd13b89f4a89835c3
hjs6877 <hjs6877@gmail.com>
2022-07-12 01:40:24 +0900

    Modify EmailConfiguration

--- src/main/java/com/codestates/helper/email/EmailConfiguration.java
+++ src/main/java/com/codestates/helper/email/EmailConfiguration.java
@@ -2,12 +2,20 @@ package com.codestates.helper.email;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
 public class EmailConfiguration {
+
+    @Primary
     @Bean
-    public EmailSendable emailSendable() {
+    public EmailSendable mockExceptionEmailSendable() {
         return new MockExceptionEmailSendable();
+    }
+
+    @Bean
+    public EmailSendable simpleEmailSendable() {
+        return new SimpleEmailSendable();
     }
 }
