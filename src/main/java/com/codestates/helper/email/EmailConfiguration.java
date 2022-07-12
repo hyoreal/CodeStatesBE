971665bb157039921f7272e0d5cbfed2fe130c8f
hjs6877 <hjs6877@gmail.com>
2022-07-12 09:58:08 +0900

    Test @Primary

--- src/main/java/com/codestates/helper/email/EmailConfiguration.java
+++ src/main/java/com/codestates/helper/email/EmailConfiguration.java
@@ -2,11 +2,18 @@ package com.codestates.helper.email;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class EmailConfiguration {
+    @Primary
     @Bean
-    public EmailSendable emailSendable() {
+    public EmailSendable MockEmailSendable() {
         return new MockEmailSendable();
+    }
+
+    @Bean
+    public EmailSendable SimpleEmailSendable() {
+        return new SimpleEmailSendable();
     }
 }
