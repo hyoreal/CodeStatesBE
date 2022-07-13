bd2c191b7ade90bb10e09bf59d436c7f19160b4a
hjs6877 <hjs6877@gmail.com>
2022-07-13 16:12:37 +0900

    Solve StampCalculatorTest

--- src/test/java/com/codestates/homework/StampCalculatorTest.java
+++ src/test/java/com/codestates/homework/StampCalculatorTest.java
@@ -1,8 +1,14 @@
 package com.codestates.homework;
 
 import com.codestates.helper.StampCalculator;
+import com.codestates.order.entity.Order;
+import com.codestates.order.entity.OrderCoffee;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StampCalculatorTest {
     @Test
@@ -15,15 +21,30 @@ public class StampCalculatorTest {
         // when
         int actual = StampCalculator.calculateStampCount(5, 3);
 
-        int expected = 7;
+        int expected = 8;
 
         // then
-        System.out.println(expected == actual);
+        assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("실습1: 주문 후 누적 스탬프 카운트 계산 탄위 테스트")
     public void calculateEarnedStampCountTest(){
-        // TODO 여기에 테스트 케이스를 작성해주세요.
-    }
+        Order order = new Order();
+        OrderCoffee orderCoffee1 = new OrderCoffee();
+        orderCoffee1.setQuantity(3);
+
+        OrderCoffee orderCoffee2 = new OrderCoffee();
+        orderCoffee2.setQuantity(5);
+
+        order.setOrderCoffees(List.of(orderCoffee1, orderCoffee2));
+
+        int expected = orderCoffee1.getQuantity() + orderCoffee2.getQuantity();
+
+        // when
+        int actual = StampCalculator.calculateEarnedStampCount(order);
+
+
+        // then
+        assertEquals(expected, actual);    }
 }
