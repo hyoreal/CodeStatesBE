d9850a40f8da2bda00b9889c8eb74cecb6d64591
hjs6877 <hjs6877@gmail.com>
2022-07-15 10:25:19 +0900

    Add Origin solution code

--- src/test/java/com/codestates/homework/MemberControllerHomeworkTest_V2.java
+++ src/test/java/com/codestates/homework/MemberControllerHomeworkTest_V2.java
@@ -34,7 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class MemberControllerHomeworkTest implements MemberControllerTestHelper {
+public class MemberControllerHomeworkTest_V2 implements MemberControllerTestHelper {
     @Autowired
     private MockMvc mockMvc;
 
