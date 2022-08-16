14718e45b2363a78d9b53476e1578a9d467e36e3
hjs6877 <hjs6877@gmail.com>
2022-08-16 10:58:58 +0900

    Modify CoffeeController

--- src/main/java/com/codestates/coffee/CoffeeController.java
+++ src/main/java/com/codestates/coffee/CoffeeController.java
@@ -4,41 +4,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/coffees")
 public class CoffeeController {
-    @PostMapping
-    public ResponseEntity postCoffee(@RequestParam("korName") String korName,
-                                     @RequestParam("engName") String engName,
-                                     @RequestParam("price") int price) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("korName", korName);
-        map.put("engName", engName);
-        map.put("price", price);
+    private final Map<Long, Map<String, Object>> coffees = new HashMap<>();
 
+    @PostConstruct
+    public void init() {
+        Map<String, Object> coffee1 = new HashMap<>();
+        long coffeeId = 1L;
+        coffee1.put("coffeeId", coffeeId);
+        coffee1.put("korName", "바닐라 라떼");
+        coffee1.put("engName", "Vanilla Latte");
+        coffee1.put("price", 4500);
 
-        return new ResponseEntity<>(map, HttpStatus.CREATED);
-    }
-
-    @GetMapping("/{coffee-id}")
-    public ResponseEntity getCoffee(@PathVariable("coffee-id") long coffeeId) {
-        System.out.println("# coffeeId: " + coffeeId);
-
-        // not implementation
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @GetMapping
-    public ResponseEntity getCoffees() {
-        System.out.println("# get Coffees");
-
-        // not implementation
-
-        return new ResponseEntity<>(HttpStatus.OK);
+        coffees.put(coffeeId, coffee1);
     }
 
     //---------------- 여기서 부터 아래에 코드를 구현하세요! -------------------//
@@ -47,18 +31,27 @@ public class CoffeeController {
     public ResponseEntity patchCoffee(@PathVariable("coffee-id") long coffeeId,
                                       @RequestParam("korName") String korName,
                                       @RequestParam("price") int price) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("coffeeId", coffeeId);
-        body.put("korName", korName);
-        body.put("engName", "Vanilla Latte");
-        body.put("price", price);
-        return new ResponseEntity<>(body, HttpStatus.OK);
+        // No need Business logic
+
+        Map<String, Object> coffee1 = coffees.get(coffeeId);
+
+        if (coffee1 == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        } else {
+            coffee1.put("korName", korName);
+            coffee1.put("price", price);
+        }
+
+        return new ResponseEntity<>(coffee1, HttpStatus.OK);
     }
 
-    // 2. 커피피 정보 삭제를 위한 핸들러 서드 구현
+    // 2. 커피 정보 삭제를 위한 핸들러 서드 구현
     @DeleteMapping("/{coffee-id}")
     public ResponseEntity deleteCoffee(@PathVariable("coffee-id") long coffeeId) {
-        // No need business logic
+        if(coffees.containsKey(coffeeId))
+            coffees.remove(coffeeId);
+        else
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
--- src/main/java/com/codestates/member/MemberController.java
+++ src/main/java/com/codestates/member/MemberController.java
@@ -4,62 +4,51 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/members")
 public class MemberController {
-    @PostMapping
-    public ResponseEntity postMember(@RequestParam("email") String email,
-                                     @RequestParam("name") String name,
-                                     @RequestParam("phone") String phone) {
-        Map<String, String> map = new HashMap<>();
-        map.put("email", email);
-        map.put("name", name);
-        map.put("phone", phone);
+    private final Map<Long, Map<String, Object>> members = new HashMap<>();
 
-        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    @PostConstruct
+    public void init() {
+        Map<String, Object> member1 = new HashMap<>();
+        long memberId = 1L;
+        member1.put("memberId", memberId);
+        member1.put("email", "hgd@gmail.com");
+        member1.put("name", "홍길동");
+        member1.put("phone", "010-1234-5678");
+
+        members.put(memberId, member1);
     }
-
-    @GetMapping("/{member-id}")
-    public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
-        System.out.println("# memberId: " + memberId);
-
-        // not implementation
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @GetMapping
-    public ResponseEntity getMembers() {
-        System.out.println("# get Members");
-
-        // not implementation
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
 
     //---------------- 여기서 부터 아래에 코드를 구현하세요! -------------------//
     // 1. 회원 정보 수정을 위한 핸들러 메서드 구현
     @PatchMapping("/{member-id}")
     public ResponseEntity patchMember(@PathVariable("member-id") long memberId,
                                       @RequestParam String phone) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("memberId", memberId);
-        body.put("email", "hgd@gmail.com");
-        body.put("name", "홍길동");
-        body.put("phone", phone);
-
         // No need Business logic
 
-        return new ResponseEntity<>(body, HttpStatus.OK);
+        Map<String, Object> member1 = members.get(memberId);
+
+        if(member1 == null)
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        else
+            member1.put("phone", phone);
+
+        return new ResponseEntity<>(member1, HttpStatus.OK);
     }
 
     // 2. 회원 정보 삭제를 위한 핸들러 메서드 구현
     @DeleteMapping("/{member-id}")
     public ResponseEntity deleteMember(@PathVariable("member-id") long memberId) {
-        // No need business logic
+        if(members.containsKey(memberId))
+            members.remove(memberId);
+        else
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
