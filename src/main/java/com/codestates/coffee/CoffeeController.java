4e691a655d93e88731181aa6c434d2f02020c84a
hyojin <hyoreal51@gmail.com>
2022-08-22 16:01:41 +0900

    과제

--- src/main/java/com/codestates/coffee/CoffeeController.java
+++ src/main/java/com/codestates/coffee/CoffeeController.java
@@ -2,39 +2,33 @@ package com.codestates.coffee;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import java.util.HashMap;
 import java.util.Map;
 
+@Validated
 @RestController
 @RequestMapping("/v1/coffees")
 public class CoffeeController {
     // 1. DTO 클래스 및 유효성 검증을 적용하세요.
     @PostMapping
-    public ResponseEntity postCoffee(@RequestParam("korName") String korName,
-                                     @RequestParam("engName") String engName,
-                                     @RequestParam("price") int price) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("korName", korName);
-        map.put("engName", engName);
-        map.put("price", price);
+    public ResponseEntity postCoffee(@Valid @RequestBody CoffeePostDto coffeePostDto) {
 
-
-        return new ResponseEntity<>(map, HttpStatus.CREATED);
+        return new ResponseEntity<>(coffeePostDto, HttpStatus.CREATED);
     }
 
     // 2. DTO 클래스 및 유효성 검증을 적용하세요.
     @PatchMapping("/{coffee-id}")
-    public ResponseEntity patchCoffee(@PathVariable("coffee-id") long coffeeId,
-                                      @RequestParam("korName") String korName,
-                                      @RequestParam("price") int price) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("coffeeId", coffeeId);
-        body.put("korName", korName);
-        body.put("engName", "Vanilla Latte");
-        body.put("price", price);
-        return new ResponseEntity<>(body, HttpStatus.OK);
+    public ResponseEntity patchCoffee(@PathVariable("coffee-id") @Min(1) long coffeeId,
+                                      @RequestBody @Valid CoffeePatchDto coffeePatchDto) {
+
+        coffeePatchDto.setCoffeeId(coffeeId);
+
+        return new ResponseEntity<>(coffeePatchDto, HttpStatus.OK);
     }
 
     @GetMapping("/{coffee-id}")
---
+++ src/main/java/com/codestates/coffee/CoffeePostDto.java
@@ -0,0 +1,45 @@
+package com.codestates.coffee;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+public class CoffeePostDto {
+
+    @NotBlank(message = "공백만으로 구성되지 않아야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*$")
+    private String korName;
+
+    @NotBlank(message = "공백만으로 구성되지 않아야 합니다.")
+    @Pattern(regexp = "^\\w+\\s?\\w*$", message = "워드 사이에 한칸의 공백만 허용될 수 있습니다.")
+    private String engName;
+
+    @Min(value = 100, message = "100이상의 숫자만 허용합니다.")
+    @Max(value = 50000, message = "50000이하의 숫자만 허용합니다.")
+    private int price;
+
+    public String getKorName() {
+        return korName;
+    }
+
+    public void setKorName(String korName) {
+        this.korName = korName;
+    }
+
+    public String getEngName() {
+        return engName;
+    }
+
+    public void setEngName(String engName) {
+        this.engName = engName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
