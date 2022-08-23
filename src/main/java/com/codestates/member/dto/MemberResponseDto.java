dfb2bfc1593de637b869d5986f188c55375cb986
hjs6877 <hjs6877@gmail.com>
2022-08-23 21:39:39 +0900

    Add @Builder in MemberResponseDto

--- src/main/java/com/codestates/member/dto/MemberResponseDto.java
+++ src/main/java/com/codestates/member/dto/MemberResponseDto.java
@@ -1,10 +1,8 @@
 package com.codestates.member.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
