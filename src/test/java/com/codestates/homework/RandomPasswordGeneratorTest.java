3acccc9419d13b21480e0518d43a731209724118
hyojin <hyoreal51@gmail.com>
2022-09-07 16:35:48 +0900

    StampCalculator, RandomPasswordGenerator 단위 테스트 케이스 실습 과제 complete

--- src/test/java/com/codestates/homework/RandomPasswordGeneratorTest.java
+++ src/test/java/com/codestates/homework/RandomPasswordGeneratorTest.java
@@ -4,13 +4,50 @@ import com.codestates.helper.RandomPasswordGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RandomPasswordGeneratorTest {
     @DisplayName("실습 3: 랜덤 패스워드 생성 테스트")
     @Test
     public void generateTest() {
         // TODO 여기에 테스트 케이스를 작성해주세요.
+
+        // given
+        int numberOfUpperCaseLetters = 2;
+        int numberOfLowerCaseLetters = 5;
+        int numberOfNumeric = 2;
+        int numberOfSpecialChars = 1;
+        int pWLength = numberOfUpperCaseLetters +
+                numberOfLowerCaseLetters +
+                numberOfNumeric +
+                numberOfSpecialChars;
+
+        // when
+        String randomPW = RandomPasswordGenerator.generate(
+                numberOfUpperCaseLetters,
+                numberOfLowerCaseLetters,
+                numberOfNumeric,
+                numberOfSpecialChars);
+
+        int randomPWLength = randomPW.length();
+        int randomPWUpperCaseLetters = (int) randomPW.chars()
+                        .filter(Character::isUpperCase)
+                        .count();
+        int randomPWLowerCaseLetters = (int) randomPW.chars()
+                        .filter(Character::isLowerCase)
+                        .count();
+        int randomPWNumeric = (int) randomPW.chars()
+                        .filter(Character::isDigit)
+                        .count();
+        int randomPWSpecialChars = (int) randomPW.chars()
+                        .filter(c -> c>= 33 && c <= 47)
+                        .count();
+
+        // then
+        assertEquals(pWLength, randomPWLength);
+        assertEquals(numberOfUpperCaseLetters, randomPWUpperCaseLetters);
+        assertEquals(numberOfLowerCaseLetters, randomPWLowerCaseLetters);
+        assertEquals(numberOfNumeric, randomPWNumeric);
+        assertEquals(numberOfSpecialChars, randomPWSpecialChars);
     }
 }
--- src/test/java/com/codestates/homework/StampCalculatorTest.java
+++ src/test/java/com/codestates/homework/StampCalculatorTest.java
@@ -1,18 +1,51 @@
 package com.codestates.homework;
 
+import com.codestates.helper.StampCalculator;
+import com.codestates.order.entity.Order;
+import com.codestates.order.entity.OrderCoffee;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class StampCalculatorTest {
     @Test
     @DisplayName("실습1: 스탬프 카운트 계산 단위 테스트")
     public void calculateStampCountTest() {
         // TODO 여기에 테스트 케이스를 작성해주세요.
+        // given
+        int nowCount = 5;
+        int earned = 3;
+
+        // when
+        int actual = StampCalculator.calculateStampCount(nowCount, earned);
+        int expected = 8;
+
+        // then
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("실습1: 주문 후 누적 스탬프 카운트 계산 단위 테스트")
     public void calculateEarnedStampCountTest(){
         // TODO 여기에 테스트 케이스를 작성해주세요.
+        // given
+        Order order = new Order();
+        OrderCoffee orderCoffee1 = new OrderCoffee();
+        OrderCoffee orderCoffee2 = new OrderCoffee();
+        orderCoffee1.setQuantity(3);
+        orderCoffee2.setQuantity(5);
+        order.setOrderCoffees(List.of(orderCoffee1, orderCoffee2));
+
+        // when
+        int result = order.getOrderCoffees().stream()
+                .map(OrderCoffee::getQuantity)
+                .mapToInt(quantity -> quantity)
+                .sum();
+        int expected = 8;
+
+        // then
+        Assertions.assertEquals(result, expected);
     }
 }
