1227e283e5f3f187a0d23272b4495d638c3e8747
hjs6877 <hjs6877@gmail.com>
2022-09-16 11:27:41 +0900

    a reference project for JWT basic

---
+++ src/main/java/com/codestates/Section4Week1SpringSecurityJwtBasicApplication.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section4Week1SpringSecurityJwtBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section4Week1SpringSecurityJwtBasicApplication.class, args);
+	}
+
+}
