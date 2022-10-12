b1688eaec0e74e6354fafe8d0ea040ef73a0a376
hjs6877 <hjs6877@gmail.com>
2022-10-12 18:35:50 +0900

    Modify CoffeeMapper

--- src/main/java/com/codestates/coffee/mapper/CoffeeMapper.java
+++ src/main/java/com/codestates/coffee/mapper/CoffeeMapper.java
@@ -5,11 +5,14 @@ import com.codestates.coffee.dto.CoffeePostDto;
 import com.codestates.coffee.dto.CoffeeResponseDto;
 import com.codestates.coffee.entity.Coffee;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper
 public interface CoffeeMapper {
+    CoffeeMapper INSTANCE = Mappers.getMapper(CoffeeMapper.class);
+
     Coffee coffeePostDtoToCoffee(CoffeePostDto coffeePostDto);
     Coffee coffeePatchDtoToCoffee(CoffeePatchDto coffeePatchDto);
     CoffeeResponseDto coffeeToCoffeeResponseDto(Coffee coffee);
