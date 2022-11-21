b3100215ec4d5b3dc7a1342d1d161d69c20b6168
hjs6877 <hjs6877@gmail.com>
2022-11-21 11:34:09 +0900

    .

--- src/test/java/com/codestates/PasswordEncoderTest.java
+++ src/test/java/com/codestates/PasswordEncoderTest.java
@@ -8,6 +8,9 @@ import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class PasswordEncoderTest {
     @Test
     public void encodePasswordTest() {
@@ -19,24 +22,32 @@ public class PasswordEncoderTest {
         encoders.put("scrypt", new SCryptPasswordEncoder());
         encoders.put("sha256", new StandardPasswordEncoder());
 
+        String message = "hello";
+
         PasswordEncoder passwordEncoder = new DelegatingPasswordEncoder(idForEncode, encoders);
-        String bcryptPassword = passwordEncoder.encode("hello");
+        String bcryptPassword = passwordEncoder.encode(message);
         System.out.println("bcrypt encoded: " + bcryptPassword);
 
         passwordEncoder = new DelegatingPasswordEncoder("noop", encoders);
-        String noopPassword = passwordEncoder.encode("hello");
+        String noopPassword = passwordEncoder.encode(message);
         System.out.println("noop encoded: " + noopPassword);
 
         passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-        String pbkdf2Password = passwordEncoder.encode("hello");
-        System.out.println("pbkdf2Password encoded: " + pbkdf2Password);
+        String pbkdf2Password = passwordEncoder.encode(message);
+        System.out.println("pbkdf2 encoded: " + pbkdf2Password);
 
         passwordEncoder = new DelegatingPasswordEncoder("scrypt", encoders);
-        String scryptPassword = passwordEncoder.encode("hello"); // NoClassesDefFoundError 발생!
-        System.out.println("scryptPassword encoded: " + scryptPassword);
+        String scryptPassword = passwordEncoder.encode(message); // NoClassesDefFoundError 발생!
+        System.out.println("scrypt encoded: " + scryptPassword);
 
         passwordEncoder = new DelegatingPasswordEncoder("sha256", encoders);
-        String sha256Password = passwordEncoder.encode("hello");
-        System.out.println("sha256Password encoded: " + sha256Password);
+        String sha256Password = passwordEncoder.encode(message);
+        System.out.println("sha256 encoded: " + sha256Password);
+
+        assertThat(true, is(passwordEncoder.matches(message, bcryptPassword)));
+        assertThat(true, is(passwordEncoder.matches(message, noopPassword)));
+        assertThat(true, is(passwordEncoder.matches(message, pbkdf2Password)));
+        assertThat(true, is(passwordEncoder.matches(message, scryptPassword)));
+        assertThat(true, is(passwordEncoder.matches(message, sha256Password)));
     }
 }
