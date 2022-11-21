f7178da1ed509ca344b9661a5bb2324a97552db2
hjs6877 <hjs6877@gmail.com>
2022-11-21 18:27:12 +0900

    .

--- src/main/java/com/codestates/Section3Week1SolutionControllerApplication.java
+++ src/main/java/com/codestates/Section3Week1SolutionControllerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Be39Section3Week1SolutionControllerApplication {
+public class Section3Week1SolutionControllerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Be39Section3Week1SolutionControllerApplication.class, args);
+		SpringApplication.run(Section3Week1SolutionControllerApplication.class, args);
 	}
 
 }
