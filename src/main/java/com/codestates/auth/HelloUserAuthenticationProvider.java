dee50b1c66c1cc5e48b75b8dc73baa03ed8e08de
hjs6877 <hjs6877@gmail.com>
2022-12-02 11:16:47 +0900

    Modify HelloUserAuthenticationProvider

--- src/main/java/com/codestates/auth/HelloUserAuthenticationProvider.java
+++ src/main/java/com/codestates/auth/HelloUserAuthenticationProvider.java
@@ -1,14 +1,12 @@
 package com.codestates.auth;
 
-import com.codestates.auth.utils.HelloAuthorityUtils;
-import com.codestates.member.Member;
-import com.codestates.member.MemberService;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -18,15 +16,12 @@ import java.util.Optional;
 
 @Component
 public class HelloUserAuthenticationProvider implements AuthenticationProvider {
-    private final MemberService memberService;
-    private final HelloAuthorityUtils authorityUtils;
+    private final HelloUserDetailsServiceV3 userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
-    public HelloUserAuthenticationProvider(MemberService memberService,
-                                           HelloAuthorityUtils authorityUtils,
+    public HelloUserAuthenticationProvider(HelloUserDetailsServiceV3 userDetailsService,
                                            PasswordEncoder passwordEncoder) {
-        this.memberService = memberService;
-        this.authorityUtils = authorityUtils;
+        this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -36,14 +31,14 @@ public class HelloUserAuthenticationProvider implements AuthenticationProvider {
 //
 //        String username = authToken.getName();
 //        Optional.ofNullable(username).orElseThrow(() -> new UsernameNotFoundException("Invalid User name or User Password"));
-//        Member member = memberService.findMember(username);
+//        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 //
-//        String password = member.getPassword();
+//        String password = userDetails.getPassword();
 //        verifyCredentials(authToken.getCredentials(), password);
 //
-//        Collection<? extends GrantedAuthority> authorities = authorityUtils.createAuthorities(member.getRoles());
+//        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
 //
-//        return new UsernamePasswordAuthenticationToken(username, password, authorities);
+//        return UsernamePasswordAuthenticationToken.authenticated(username, password, authorities);
 //    }
 
     // V2: AuthenticationException을 rethrow 하는 개선 코드
@@ -54,12 +49,12 @@ public class HelloUserAuthenticationProvider implements AuthenticationProvider {
         String username = authToken.getName();
         Optional.ofNullable(username).orElseThrow(() -> new UsernameNotFoundException("Invalid User name or User Password"));
         try {
-            Member member = memberService.findMember(username);
-            String password = member.getPassword();
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            String password = userDetails.getPassword();
             verifyCredentials(authToken.getCredentials(), password);
 
-            Collection<? extends GrantedAuthority> authorities = authorityUtils.createAuthorities(member.getRoles());
-            return new UsernamePasswordAuthenticationToken(username, password, authorities);
+            Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+            return UsernamePasswordAuthenticationToken.authenticated(username, password, authorities);
         } catch (Exception ex) {
             throw new UsernameNotFoundException(ex.getMessage());
         }
