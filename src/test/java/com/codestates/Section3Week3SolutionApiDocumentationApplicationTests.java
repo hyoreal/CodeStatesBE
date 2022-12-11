1f744c15f71bb02f8673f663ea26f767019ac559
hjs6877 <hjs6877@gmail.com>
2022-12-12 08:54:54 +0900

    .

--- src/test/java/com/codestates/Section3Week3SolutionApiDocumentationApplicationTests.java
+++ src/test/java/com/codestates/Section3Week3SolutionApiDocumentationApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Be39Section3Week3SolutionApiDocumentationApplicationTests {
+class Section3Week3SolutionApiDocumentationApplicationTests {
 
 	@Test
 	void contextLoads() {
