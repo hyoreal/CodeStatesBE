76dd626484caa0ef8ac5648c1192559123c4d3c6
hjs6877 <hjs6877@gmail.com>
2022-12-19 00:04:37 +0900

    CoffeePatchDto

--- src/main/java/com/codestates/coffee/dto/CoffeePatchDto.java
+++ src/main/java/com/codestates/coffee/dto/CoffeePatchDto.java
@@ -15,37 +15,22 @@ public class CoffeePatchDto {
     @Pattern(regexp = "^([A-Za-z])(\\s?[A-Za-z])*$", message = "커피명(영문)은 영문이어야 합니다. 예) Cafe Latte")
     private String engName;
 
-    private Optional<@Range(min= 100, max= 50000) Integer> price = Optional.empty();
+    @Range(min= 100, max= 50000)
+    private Integer price;
 
     public long getCoffeeId() {
         return coffeeId;
-    }
-
-    public void setCoffeeId(long coffeeId) {
-        this.coffeeId = coffeeId;
     }
 
     public String getKorName() {
         return korName;
     }
 
-    public void setKorName(String korName) {
-        this.korName = korName;
-    }
-
     public String getEngName() {
         return engName;
     }
 
-    public void setEngName(String engName) {
-        this.engName = engName;
-    }
-
-    public int getPrice() {
-        return price.orElse(0);
-    }
-
-    public void setPrice(Optional<Integer> price) {
-        this.price = price;
+    public Integer getPrice() {
+        return price;
     }
 }
