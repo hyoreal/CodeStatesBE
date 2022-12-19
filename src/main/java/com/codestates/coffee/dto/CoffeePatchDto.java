a7e9dc0faa3a7a6ca48c8c1d812b53f9a3531eb6
hjs6877 <hjs6877@gmail.com>
2022-12-19 11:26:21 +0900

    .

--- src/main/java/com/codestates/coffee/dto/CoffeePatchDto.java
+++ src/main/java/com/codestates/coffee/dto/CoffeePatchDto.java
@@ -4,7 +4,6 @@ import com.codestates.validator.NotSpace;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.Pattern;
-import java.util.Optional;
 
 public class CoffeePatchDto {
     private long coffeeId;
@@ -15,7 +14,8 @@ public class CoffeePatchDto {
     @Pattern(regexp = "^([A-Za-z])(\\s?[A-Za-z])*$", message = "커피명(영문)은 영문이어야 합니다. 예) Cafe Latte")
     private String engName;
 
-    private Optional<@Range(min= 100, max= 50000) Integer> price = Optional.empty();
+    @Range(min= 100, max= 50000)
+    private Integer price;
 
     public long getCoffeeId() {
         return coffeeId;
@@ -29,23 +29,11 @@ public class CoffeePatchDto {
         return korName;
     }
 
-    public void setKorName(String korName) {
-        this.korName = korName;
-    }
-
     public String getEngName() {
         return engName;
     }
 
-    public void setEngName(String engName) {
-        this.engName = engName;
-    }
-
-    public int getPrice() {
-        return price.orElse(0);
-    }
-
-    public void setPrice(Optional<Integer> price) {
-        this.price = price;
+    public Integer getPrice() {
+        return price;
     }
 }
