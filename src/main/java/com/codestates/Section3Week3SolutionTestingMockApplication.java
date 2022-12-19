86ba05f3980aa29c21f24bc71aa53d0419161971
hjs6877 <hjs6877@gmail.com>
2022-12-19 12:16:07 +0900

    .

--- src/main/java/com/codestates/Section3Week3SolutionTestingMockApplication.java
+++ src/main/java/com/codestates/Section3Week3SolutionTestingMockApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class Be39Section3Week3SolutionTestingMockApplication {
+public class Section3Week3SolutionTestingMockApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Be39Section3Week3SolutionTestingMockApplication.class, args);
+		SpringApplication.run(Section3Week3SolutionTestingMockApplication.class, args);
 	}
 
 }
