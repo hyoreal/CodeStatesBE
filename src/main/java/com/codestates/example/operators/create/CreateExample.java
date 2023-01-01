e500ab2bb573a8d6e05cd8649b8c2ca20fb21cde
hyojin <hyoreal51@gmail.com>
2023-01-01 16:45:30 +0900

    commit

---
+++ src/main/java/com/codestates/example/operators/create/CreateExample.java
@@ -0,0 +1,28 @@
+package com.codestates.example.operators.create;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+* create() Operator 기본 예제
+* */
+@Slf4j
+public class CreateExample {
+    private static final List<Integer> source = Arrays.asList(1, 3, 5, 7, 9, 11, 13, 15, 17, 19);
+
+    public static void main(String[] args) {
+        Flux.create((FluxSink<Integer> sink) -> {
+            sink.onRequest(n -> {
+                for (Integer integer : source) {
+                    sink.next(integer);
+                }
+                sink.complete();
+            });
+            sink.onDispose(() -> log.info("# clean up"));
+        }).subscribe(data -> log.info("# onNext: {}", data));
+    }
+}
---
+++ src/main/java/com/codestates/example/operators/error/ErrorExample01.java
@@ -0,0 +1,24 @@
+package com.codestates.example.operators.error;
+
+import com.codestates.example.operators.sample_data.Coffee;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+/*
+* error 기본 예제
+* */
+@Slf4j
+public class ErrorExample01 {
+    public static void main(String[] args) {
+        Mono.justOrEmpty(findVerifiedCoffee())
+                .switchIfEmpty(Mono.error(new RuntimeException("Not found coffee")))
+                .subscribe(
+                        data -> log.info("{} : {}", data.getKorName(), data.getPrice()),
+                        error -> log.error("# onError: {}", error.getMessage()));
+    }
+
+    private static Coffee findVerifiedCoffee() {
+        // TODO 데이터베이스에서 Coffee 정보를 조회할 수 있습니다.
+        return null;
+    }
+}
